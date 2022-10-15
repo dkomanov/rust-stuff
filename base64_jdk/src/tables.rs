9daaadd43fc76b821614a318e3169eb3d2303371
Dmitry Komanov <dmitryk@wix.com>
2022-10-15 17:05:12 +0300

    Remove unused use

--- base64_jdk/src/tables.rs
+++ base64_jdk/src/tables.rs
@@ -654,8 +654,6 @@ pub const FROM_BASE64_URL_SAFE: &DecodeTable = &[
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     const BASE64_STANDARD_ALPHABET: &[char; 64] = &[
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
